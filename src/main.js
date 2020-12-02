3f32549960ab5fec9327c08132997f0b9f46c217
Louise Lin <yishinlin@outlook.com>
2020-12-02 14:57:26 +0800

    接上Json Server

--- src/main.js
+++ src/main.js
@@ -2,14 +2,43 @@ import Vue from "vue";
 import App from "./App.vue";
 import Vuex from "vuex";
 import VueRouter from "vue-router";
-import routers from "./routers";
+import List from "./components/list.vue";
+import axios from "axios";
 
 Vue.use(Vuex);
 Vue.use(VueRouter);
 
+let myRouter = [
+  {
+    path: "/",
+    name: "list",
+    component: List
+  },
+
+  {
+    path: "*",
+    redirect: "/"
+  }
+];
+
+let routers = new VueRouter({ routes: myRouter, mode: "history" });
 let store = new Vuex.Store({
   state: {
     contents: []
+  },
+  mutations: {
+    setContents(state, data) {
+      state.contents = data;
+    }
+  },
+  actions: {
+    CONTENTS_READ: (context) => {
+      return axios
+        .get("https://lqrqc.sse.codesandbox.io/products")
+        .then((res) => {
+          context.commit("setContents", res.data);
+        });
+    }
   }
 });
 
--- src/routers.js
+++
@@ -1,7 +0,0 @@
-import VueRouter from "vue-router";
-
-export const routes = new VueRouter([
-  {
-    path: ""
-  }
-]);
